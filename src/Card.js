07d004552d3b39040fe996fc0f65c8000b09e1d6
nieylarm <nieylarm@gmail.com>
2017-10-22 21:30:04 +0800

    Updated

--- src/Card.js
+++ src/Card.js
@@ -12,8 +12,8 @@ class Card extends Component {
         <img className="card-img-top" src={this.props.imgSrc} alt={this.props.imgAlt} />
         <div className="card-body">
           <h4 className="card-title">{this.props.title}</h4>
-          <p className="card-text">{this.props.text}</p>
           <p className="card-text"><small className="text-muted">{this.props.updated}</small></p>
+          <p className="card-text">{this.props.text}</p>
         </div>
       </div>
     );
